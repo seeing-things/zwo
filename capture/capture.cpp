b0713c485d6ba1800b3104a70dccd26ad1bcd3c3
Brett Gottula <bgottula@gmail.com>
2018-11-01 15:40:58 -0700

    #1: Adds live preview using OpenCV

--- capture/capture.cpp
+++ capture/capture.cpp
@@ -13,6 +13,8 @@
 #include <sys/syscall.h>
 #include <fcntl.h>
 #include <err.h>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
 #include "ASICamera2.h"
 
 
@@ -32,10 +34,12 @@ atomic_bool exposure_updated = false;
 
 // std::deque is not thread safe
 mutex to_disk_deque_mutex;
+mutex to_preview_deque_mutex;
 mutex to_agc_deque_mutex;
 mutex unused_deque_mutex;
 
 condition_variable to_disk_deque_cv;
+condition_variable to_preview_deque_cv;
 condition_variable to_agc_deque_cv;
 condition_variable unused_deque_cv;
 
@@ -43,6 +47,7 @@ class Frame;
 
 // FIFOs holding pointers to frame objects
 deque<Frame *> to_disk_deque;
+deque<Frame *> to_preview_deque;
 deque<Frame *> to_agc_deque;
 deque<Frame *> unused_deque;
 
@@ -153,6 +158,7 @@ void sigint_handler(int signal)
 {
     end_program = true;
     to_disk_deque_cv.notify_one();
+    to_preview_deque_cv.notify_one();
     to_agc_deque_cv.notify_one();
     unused_deque_cv.notify_one();
 }
@@ -193,6 +199,59 @@ void write_to_disk(int fd)
     (void)close(fd);
 
     printf("Disk thread ending.\n");
+}
+
+
+void preview()
+{
+    constexpr char WINDOW_NAME[] = "Live Preview";
+
+    printf("preview thread id: %ld\n", syscall(SYS_gettid));
+
+    cv::namedWindow(WINDOW_NAME, cv::WINDOW_NORMAL);
+    cv::resizeWindow(WINDOW_NAME, 640, 480);
+
+    while (!end_program)
+    {
+        // Get frame from deque
+        unique_lock<mutex> to_preview_deque_lock(to_preview_deque_mutex);
+        to_preview_deque_cv.wait(to_preview_deque_lock, [&]{return !to_preview_deque.empty() || end_program;});
+        if (end_program)
+        {
+            break;
+        }
+        while (to_preview_deque.size() > 1)
+        {
+            // Discard all but most recent frame
+            to_preview_deque.back()->decrRefCount();
+            to_preview_deque.pop_back();
+        }
+        Frame *frame = to_preview_deque.back();
+        to_preview_deque.pop_back();
+        to_preview_deque_lock.unlock();
+
+        try
+        {
+            // This throws an exception if the window is closed
+            cv::getWindowProperty(WINDOW_NAME, 0);
+        }
+        catch (cv::Exception &e)
+        {
+            // window was closed by user
+            frame->decrRefCount();
+            break;
+        }
+
+        cv::Mat img_bayer_bg(2080, 3096, CV_8UC1, (void *)(frame->frame_buffer_));
+        cv::Mat img_bgr;
+        cv::cvtColor(img_bayer_bg, img_bgr, CV_BayerBG2BGR);
+        cv::imshow(WINDOW_NAME, img_bgr);
+        cv::waitKey(1);
+
+        frame->decrRefCount();
+    }
+
+    printf("Preview thread ending.\n");
 }
 
 
@@ -393,10 +452,9 @@ int main()
         err(1, "open(%s) failed", FILE_NAME);
     }
 
-    // start thread for writing frame data to disk
+    // start threads
     static thread write_to_disk_thread(write_to_disk, fd);
-
-    // start thread for AGC
+    static thread preview_thread(preview);
     static thread agc_thread(agc);
 
     asi_rtn = ASIStartVideoCapture(CamInfo.CameraID);
@@ -475,6 +533,16 @@ int main()
                 to_agc_deque_cv.notify_one();
             }
 
+            // Put this frame in the deque headed for live preview thread if the deque is empty
+            if (to_preview_deque.empty())
+            {
+                frame->incrRefCount();
+                unique_lock<mutex> to_preview_deque_lock(to_preview_deque_mutex);
+                to_preview_deque.push_front(frame);
+                to_preview_deque_lock.unlock();
+                to_preview_deque_cv.notify_one();
+            }
+
             // Put this frame in the deque headed for write to disk thread
             unique_lock<mutex> to_disk_deque_lock(to_disk_deque_mutex);
             to_disk_deque.push_front(frame);
@@ -510,6 +578,7 @@ int main()
     ASICloseCamera(CamInfo.CameraID);
 
     write_to_disk_thread.join();
+    preview_thread.join();
     agc_thread.join();
 
     printf("Main thread ending.\n");
