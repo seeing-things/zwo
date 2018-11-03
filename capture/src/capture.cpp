69d3d3894dd48e3451249433a805248435ce7c1a
Brett Gottula <bgottula@gmail.com>
2018-11-03 14:08:24 -0700

    #1: Moves preview function to separate header/source file

--- capture/src/capture.cpp
+++ capture/src/capture.cpp
@@ -10,13 +10,12 @@
 #include <unistd.h>
 #include <sys/syscall.h>
 #include <err.h>
-#include <opencv2/highgui.hpp>
-#include <opencv2/imgproc.hpp>
 #include "ASICamera2.h"
 #include "Frame.h"
 #include "SERFile.h"
 #include "agc.h"
 #include "disk.h"
+#include "preview.h"
 
 
 // Defined by libASICamera2, returns a tick in milliseconds.
@@ -103,59 +102,6 @@ void sigint_handler(int signal)
     to_preview_deque_cv.notify_one();
     to_agc_deque_cv.notify_one();
     unused_deque_cv.notify_one();
-}
-
-
-void preview()
-{
-    constexpr char WINDOW_NAME[] = "Live Preview";
-
-    printf("preview thread id: %ld\n", syscall(SYS_gettid));
-
-    cv::namedWindow(WINDOW_NAME, cv::WINDOW_NORMAL);
-    cv::resizeWindow(WINDOW_NAME, 640, 480);
-
-    while (!end_program)
-    {
-        // Get frame from deque
-        unique_lock<mutex> to_preview_deque_lock(to_preview_deque_mutex);
-        to_preview_deque_cv.wait(to_preview_deque_lock, [&]{return !to_preview_deque.empty() || end_program;});
-        if (end_program)
-        {
-            break;
-        }
-        while (to_preview_deque.size() > 1)
-        {
-            // Discard all but most recent frame
-            to_preview_deque.back()->decrRefCount();
-            to_preview_deque.pop_back();
-        }
-        Frame *frame = to_preview_deque.back();
-        to_preview_deque.pop_back();
-        to_preview_deque_lock.unlock();
-
-        try
-        {
-            // This throws an exception if the window is closed
-            cv::getWindowProperty(WINDOW_NAME, 0);
-        }
-        catch (cv::Exception &e)
-        {
-            // window was closed by user
-            frame->decrRefCount();
-            break;
-        }
-
-        cv::Mat img_bayer_bg(2080, 3096, CV_8UC1, (void *)(frame->frame_buffer_));
-        cv::Mat img_bgr;
-        cv::cvtColor(img_bayer_bg, img_bgr, CV_BayerBG2BGR);
-        cv::imshow(WINDOW_NAME, img_bgr);
-        cv::waitKey(1);
-
-        frame->decrRefCount();
-    }
-
-    printf("Preview thread ending.\n");
 }
 
 
