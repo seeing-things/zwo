e74d4483963781aca8a070ecb4fa1fc9b988092b
Justin Gottula <justin@jgottula.com>
2020-02-08 17:47:32 -0800

    Set errno after pthread call
    The pthread library doesn't actually set errno; it merely returns errno
    values upon errors. This fixup should make the call to err(3) print the
    correct message when an error occurs.

--- capture/src/capture.cpp
+++ capture/src/capture.cpp
@@ -58,7 +58,7 @@ static void set_thread_priority(pthread_t thread, int policy, int priority)
 {
     sched_param sch_params;
     sch_params.sched_priority = priority;
-    if (pthread_setschedparam(thread, policy, &sch_params))
+    if ((errno = pthread_setschedparam(thread, policy, &sch_params)))
     {
         err(1, "Failed to set thread priority");
     }
