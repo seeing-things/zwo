ad689ec2f739c5619d31206a9ccce8c2484196c9
Brett Gottula <bgottula@gmail.com>
2020-11-07 15:12:18 -0800

    Minor changes to Python video preview app

--- python/video_preview.py
+++ python/video_preview.py
@@ -16,21 +16,23 @@ def main():
     asi.ASISetROIFormat(info.CameraID, info.MaxWidth, info.MaxHeight, 1, asi.ASI_IMG_RAW8)
     asi.ASISetControlValue(info.CameraID, asi.ASI_BANDWIDTHOVERLOAD, 94, asi.ASI_FALSE)
     asi.ASISetControlValue(info.CameraID, asi.ASI_HIGH_SPEED_MODE, 1, asi.ASI_FALSE)
-    asi.ASISetControlValue(info.CameraID, asi.ASI_EXPOSURE, 10000, asi.ASI_FALSE)
-    asi.ASISetControlValue(info.CameraID, asi.ASI_GAIN, 300, asi.ASI_FALSE)
+    asi.ASISetControlValue(info.CameraID, asi.ASI_EXPOSURE, 16667, asi.ASI_FALSE)
+    asi.ASISetControlValue(info.CameraID, asi.ASI_GAIN, 100, asi.ASI_FALSE)
     asi.ASIStartVideoCapture(info.CameraID)
 
-    cv2.namedWindow('video', cv2.WINDOW_NORMAL);
-    cv2.resizeWindow('video', 640, 480);
+    cv2.namedWindow('video', cv2.WINDOW_NORMAL)
+    cv2.resizeWindow('video', 640, 480)
 
     frame_count = 0
     while True:
-        print('frame {:06d}'.format(frame_count))
-        (rtn, frame) = asi.ASIGetVideoData(info.CameraID, frame_size, 200)
-        frame = np.reshape(frame, (info.MaxHeight, info.MaxWidth))
-        frame = cv2.cvtColor(frame, cv2.COLOR_BAYER_BG2BGR)
-        cv2.imshow('video', frame)
-        cv2.waitKey(1)
+        dropped_frame_count = asi.ASICheck(asi.ASIGetDroppedFrames(info.CameraID))
+        print(f'frame {frame_count:06d}, dropped: {dropped_frame_count:06d}')
+        (rtn, frame) = asi.ASIGetVideoData(info.CameraID, frame_size, 0)
+        if frame_count % 4 == 0:
+            frame = np.reshape(frame, (info.MaxHeight, info.MaxWidth))
+            frame = cv2.cvtColor(frame, cv2.COLOR_BAYER_BG2BGR)
+            cv2.imshow('video', frame)
+            cv2.waitKey(1)
         frame_count += 1
 
 if __name__ == "__main__":
